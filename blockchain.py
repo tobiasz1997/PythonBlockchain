71cfc2f177bdb1db806c0fc3731a3ef3a53d779a
Unknown <grzegorz.tobiasz97@gmail.com>
2019-10-30 14:11:53 +0100

    section 5 - Functions & Strings

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
-# print(2+66)
+import functools
+
 MINING_REWARD = 10
 
 genesis_block = {
@@ -21,15 +22,9 @@ def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_sender,0)
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_receive = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_receive += tx[0]
+    amount_receive = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_recipient,0)
     return amount_receive - amount_sent
 
 
@@ -173,7 +168,7 @@ while waiting_for_input:
         print('invalid blockchain!!!')
         break
     print('^^^^^^^^^^^^')
-    print(get_balance('Grzegorz'))
+    print('Balance of {}: {:6.2f}'.format('Grzegorz', get_balance('Grzegorz')))
 else:
     print('User left!')
 
